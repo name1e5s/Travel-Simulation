ed5f4b62862db779767c57c11d1b5f73277df1e2
name1e5s <name1e5s@qq.com>
2019-04-18 16:53:58 +0800

    Add SPFA to find the most fast route between any two cities

--- city_graph.cpp
+++ city_graph.cpp
@@ -1,11 +1,16 @@
 #include <city_graph.h>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 
+using std::deque;
+
 CityGraph::CityGraph() {
   this->init();
   this->print_edge();
+  this->floyd();
+  this->spfa();
 }
 
 CityGraph::~CityGraph() {
@@ -24,7 +29,7 @@ CityGraph::~CityGraph() {
 }
 
 // Find (and insert) cities by name
-int CityGraph::find_city(string name) {
+int CityGraph::find_city(const string &name) {
   auto res = city_index.find(name);
 
   if (res != city_index.end())
@@ -32,6 +37,7 @@ int CityGraph::find_city(string name) {
   else {
     city_index.insert(std::make_pair(name, city_num));
     index_city.insert(std::make_pair(city_num, name));
+    city[city_num].city_name = name;
     city_num++;
     return city_num - 1;
   }
@@ -130,8 +136,67 @@ void CityGraph::init(const char *path) {
   }
 }
 
+// Compute total time of a route.
+int CityGraph::compute_time(const vector<Transport> &trans, int start_time) {
+  int temp = start_time;
+  for (const auto &x : trans)
+    temp = pick_tran(temp, &(x));
+  return temp - start_time;
+}
+
+// Returns cheapest price of the given middle city sequence.
+int CityGraph::get_cheapest_price(const vector<int> &city_seq,
+                                  const Traveller &t) const {
+  if (t.middle_city_index.empty()) {
+    return cheapest_price[t.source_city_index][t.dest_city_index];
+  } else {
+    int total_price = 0;
+    for (int i = 0; i < city_seq.size() - 1; i++) {
+      total_price += cheapest_price[city_seq[i]][city_seq[i + 1]];
+    }
+  }
+}
+
+// Returns fastest time of the given middle city sequence.
+int CityGraph::get_fastest_price(const vector<int> &city_seq,
+                                 const Traveller &t,
+                                 const int begin_time) const {
+  int temp, i;
+
+  if (t.middle_city_index.empty()) {
+    return begin_time + (fastest_time[t.source_city_index][t.dest_city_index]
+                                     [begin_time % 24] -
+                         begin_time % 24);
+  } else {
+    temp = begin_time +
+           (fastest_time[t.source_city_index][city_seq[0]][begin_time % 24] -
+            (begin_time % 24));
+
+    for (i = 0; i < city_seq.size() - 1; i++)
+      temp +=
+          fastest_time[city_seq[i]][city_seq[i + 1]][temp % 24] - (temp % 24);
+    return temp + (fastest_time[city_seq[i]][t.dest_city_index][temp % 24] -
+                   (temp % 24));
+  }
+}
+
+// Returns end time when we pick the Transport EDGE at
+// begin_time. End time may be larger than 24. The function should be const.
+int CityGraph::pick_tran(int begin_time, const Transport *edge) const {
+  if (begin_time % 24 <= edge->start_time) {
+    return begin_time + (edge->start_time - begin_time % 24) + edge->duration;
+  } else { // Go next day
+    return begin_time + (edge->start_time - begin_time % 24) + edge->duration +
+           24;
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+///                    The Algorithms                          ///
+/////////////////////////////////////////////////////////////////
+
 // Pre-process the given data and compute the cheapest price using
-// the floyd algorighm.
+// the floyd algorithm.
 void CityGraph::floyd() {
   for (int k = 0; k < city_num; k++)
     for (int i = 0; i < city_num; i++)
@@ -147,4 +212,45 @@ void CityGraph::floyd() {
                                       cheapest_route[i][k].begin(),
                                       cheapest_route[i][k].end());
         }
+}
+
+// Pre-process to find the most fast route between ANY two
+// cities.
+void CityGraph::spfa() {
+  for (int i = 0; i < city_num; ++i)
+    for (int time = 0; time < 24; ++time) {
+      deque<int> deque;
+      bool used[MAX_VERT] = {false};
+
+      deque.push_back(i);
+      used[i] = true;
+
+      while (!deque.empty()) {
+        int node = deque.front();
+        Transport *temp = city[node].first_transport;
+
+        while (temp) {
+          if (fastest_time[i][temp->dest_city][time] >
+              pick_tran(fastest_time[i][node][time], temp)) {
+            fastest_time[i][temp->dest_city][time] =
+                pick_tran(fastest_time[i][node][time], temp);
+
+            fastest_route[i][temp->dest_city][time].clear();
+            fastest_route[i][temp->dest_city][time].insert(
+                fastest_route[i][temp->dest_city][time].begin(),
+                fastest_route[i][node][time].begin(),
+                fastest_route[i][node][time].end());
+            fastest_route[i][temp->dest_city][time].push_back(*temp);
+
+            if (!used[temp->dest_city]) {
+              used[temp->dest_city] = true;
+              deque.push_back(temp->dest_city);
+            }
+          }
+          temp = temp->next_transport;
+        }
+        used[node] = false;
+        deque.pop_front();
+      }
+    }
 }
